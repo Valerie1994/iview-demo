1a9b4882a874a8a39a940f21f2aeeb6d43641e61
Valerie1994 <1041797030@qq.com>
2018-07-18 14:28:50 +0800

    测试了单选框

--- src/pages/test.vue
+++ src/pages/test.vue
@@ -3,20 +3,21 @@
     <!-- back是在调用下面定义好的一个方法 -->
     <h3 @click="back">返回首页方法① 使用函数</h3>
     <router-link class="linkToIndex" :to="{ path:'/' }">返回首页方法② 使用链接</router-link>
-    <RadioGroup id="phone">
-      <Radio label="apple">
+    <RadioGroup id="phone" v-model="systemType">
+      <Radio label="apple" :value="apple">
         <Icon type="social-apple"></Icon>
         <span>Apple</span>
       </Radio>
-      <Radio label="android">
+      <Radio label="android" :value="android">
         <Icon type="social-android"></Icon>
         <span>Android</span>
       </Radio>
-      <Radio label="windows">
+      <Radio label="windows" :value="windows">
         <Icon type="social-windows"></Icon>
         <span>Windows</span>
       </Radio>
     </RadioGroup>
+    <button type="button" @click="testSubmit">打印选中的系统</button>
   </div>
 </template>
 
@@ -26,8 +27,12 @@
         methods: {
           // methods里面写各种函数方法。用给其他地方调用
           back () {
-            this.$router.push({name: 'HelloWorld'})
+            //this.$router.push({name: 'HelloWorld'});
+            this.$router.push({path: '/'});
             // 这种是函数式的跳转方法
+          },
+          testSubmit(){
+            console.log(this.systemType);
           }
         }
     }
